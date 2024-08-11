020fd883d335366c6a408ce26aa3548836e8485e
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-08-11 17:32:10 +0300

    add footer and fix bug in app component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent , FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent , FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  screenHeight:any;
+  screenWidth:any;
+  footerMaxHeight!:number;
   title = 'angularecommerce';
+
+  constructor(){
+    this.getScreenSize(event);
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event:any){
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    this.footerMaxHeight = this.screenHeight -160;
+  }
 }
