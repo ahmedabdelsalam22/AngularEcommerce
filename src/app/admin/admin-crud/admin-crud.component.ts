0be61e2831e831e20127db08b2b55f55b159985d
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-08-11 15:04:46 +0300

    create components for admin modules and create admin service

---
+++ src/app/admin/admin-crud/admin-crud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-crud.component.html',
+  styleUrl: './admin-crud.component.css'
+})
+export class AdminCrudComponent {
+
+}
